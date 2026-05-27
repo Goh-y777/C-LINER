4202f884f75c6c5539107ce90e96dcbec4bf29c6
ridcodeee <alediazro22@gmail.com>
2026-05-26 21:15:42 -0400

    Add files via upload
    Subida de los archivos necesarios para la implementacion de la migracion del lenguaje de programacion de C a C++

--- src/funciones.h
+++ src/funciones.h
@@ -1,14 +1,16 @@
-#include<stdio.h>
-#include<math.h>
-// Librerias utilizadas
+#include <iostream>
+#include <string>
 
-#define MAX_VEHICULOS 2 // Declarar la cantidad maxima de vehiculos dentro del sistema
+class Vehiculo {
+private: //Atributos con modificador de acceso privado
+    float posicion; //Atributo 1
+    float velocidad; //Atributo 2
 
-struct Vehiculo{ // Declarar la estructura Vehiculos
-    float pos; // Atributos
-    float vel;
+public: //Metodos con modificador de acceso publico
+    Vehiculo (); //Metodo constructor por defecto
+    
+    void pedirDatos(); //Metodo para pedir datos al usuario
+    void calcularEncuentroYMostrar(const Vehiculo& vehiculo, const std::string& unidad) const;
+    //Metodo para calcular el encuentro y mostrar el resultado
+    //se declara como const porque no modifica el estado del objeto actual, y recibe otro objeto Vehiculo y una unidad de medida como parámetros
 };
-// Declaracion de funciones utilizadas
-float calcularTiempo(struct Vehiculo vehiculos[]);
-float calcularDistancia(struct Vehiculo vehiculos[], float tiempo);
-void pedirDatos(struct Vehiculo vehiculos[]);
